a46f601756f98da831fd91fca3ff5ee642649c58
CapitainePigeon <37774136+CapitainePigeon@users.noreply.github.com>
2019-02-05 14:49:06 +0100

    coucou

---
+++ src/sample/console.java
@@ -0,0 +1,12 @@
+package sample;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class console implements Observer {
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
---
+++ src/sample/jeu.java
@@ -0,0 +1,11 @@
+package sample;
+
+import java.util.Observable;
+
+public class jeu extends Observable implements Runnable
+{
+    @Override
+    public void run() {
+        
+    }
+}
