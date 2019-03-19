f43493b5ab2bc2ab9188b67730bddd63663c8195
yohan <yohan.malbranque@gmail.com>
2019-03-19 18:09:15 +0100

    menu

--- src/sample/Menu.java
+++ src/sample/Menu.java
@@ -5,6 +5,8 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -13,9 +15,11 @@ public class Menu  extends Application{
     @Override
     public void start(Stage primaryStage) {
         Label label = new Label("Bienvenue sur 3pac, le pacman des bg");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+        Button play = new Button();
+        play.setText("Jouer");
+        play.setTranslateY(100);
+        play.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
@@ -23,13 +27,30 @@ public class Menu  extends Application{
             }
         });
 
+        Button quit = new Button();
+        quit.setText("Quitter");
+        quit.setTranslateY(150);
+        quit.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+
+        Image img = new Image("File:src/ressources/3pac.png", 150, 150, false,false);
+        ImageView tripac = new ImageView(img);
+        tripac.setTranslateY(-150);
+
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        root.getChildren().add(tripac);
         root.getChildren().add(label);
+        root.getChildren().add(play);
+        root.getChildren().add(quit);
 
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 350, 500);
 
-        primaryStage.setTitle("3pac - Accueil");
+        primaryStage.setTitle("3PAC - Accueil");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
